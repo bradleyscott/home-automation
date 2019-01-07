b9bffd14fac45b1d5aacca6c8b526eec9e5bfbab
Bradley <bradley.s.scott@gmail.com>
2019-01-07 20:09:22 +1300

    Added logging

--- removeOldSecurityRecordings.sh
+++ removeOldSecurityRecordings.sh
@@ -1,1 +1,6 @@
-find /media/bradley/external/security -mtime +14 -type f
+log=removeOldSecurityRecordings.log
+path=/media/bradley/external/security
+
+echo $(date) "Removing files from " $path " greater than 14 days old..." >> $log
+find $path -mtime +14 -type f -delete >> $log
+echo $(date) "Completed removal of old files" >> $log
